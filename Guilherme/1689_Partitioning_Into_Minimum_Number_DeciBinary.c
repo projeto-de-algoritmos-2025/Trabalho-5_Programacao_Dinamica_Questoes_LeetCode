a875d0169f89f9f003ce85fbf35f8bbf7a191b3b
GuilhermeMM3 <98497714+GuilhermeMM3@users.noreply.github.com>
2025-12-01 18:33:33 -0300

    Delete Guilherme/1689_Partitioning_Into_Minimum_Number_DeciBinary.c

--- Guilherme/1689_Partitioning_Into_Minimum_Number_DeciBinary.c
+++
@@ -1,8 +0,0 @@
-int minPartitions(char* n) {
-    char digito = '0';
-    for (int i = 0; n[i] != '\0'; i++){
-        if (n[i]>digito)
-            digito = n[i];
-    }
-    return digito -'0';
-}
